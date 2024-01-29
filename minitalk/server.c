43139d71e4ba12b4cb2447fa2070561dc1f0c84b
elecarlier <57401564+elecarlier@users.noreply.github.com>
2024-01-29 13:10:42 +0100

    Add files via upload

--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: ecarlier <ecarlier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 13:14:29 by ecarlier          #+#    #+#             */
-/*   Updated: 2024/01/26 16:33:42 by ecarlier         ###   ########.fr       */
+/*   Updated: 2024/01/29 13:08:42 by ecarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,28 @@
 #include <unistd.h>
 #include <signal.h>
 
-/*
-void	signal_handler(int signum)
-{
-	if (signum == SIGUSR1)
-		//;
-}
-*/
 
+void	signal_handler(int signum, siginfo_t *info)
+{
+	static int	i;
+	static __pid_t	client_pid;
+	static unsigned char	c;
+
+	c
+}
 
 int	main(void)
 {
-	pid_t	pid;
+	__pid_t	pid;
 	struct sigaction	s_sigaction;
 
 
 	pid = getpid();
 	ft_printf("The process ID is %d\n", pid);
-
+	s_sigaction.sa_sigaction = signal_handler; //pointeur vers la fct
+	s_sigaction.sa_flags = SA_SIGINFO; //utilise Siginfo pour les flags
+	sigaction(SIGUSR1, &s_sigaction, 0);
+	sigaction(SIGUSR2, &s_sigaction, 0);
 	while (1)
 	{
 		usleep(50);
